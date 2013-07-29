628cb8ae78fa0096660ff629002aa34ca4728b5d
Chris Rossi <chris@archimedeanco.com>
2013-07-29 15:36:41 -0400

    Improve docs.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 try:
-    with open(os.path.join(here, 'README.txt')) as f:
+    with open(os.path.join(here, 'README.rst')) as f:
         README = f.read()
 except IOError:
     README = ''
